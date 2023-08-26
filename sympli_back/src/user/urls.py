832bf6ce00aebc88ea510766935688fae8b501d5
Juan Cantillo M <66197856+jcantillom@users.noreply.github.com>
2023-08-26 14:22:33 -0500

    :construction: delete functions

--- sympli_back/src/user/urls.py
+++ sympli_back/src/user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserList
+from .views import UserList, UserDatail
 
 urlpatterns = [
     path('users/', UserList.as_view()),
+    path('users/<int:pk>/', UserDatail.as_view()),
 ]
